ea7394ce85b5dc9a59563ea39c786db713ea4e86
!@Murphy@!1 <tewari.nivedita@gmail.com>
2026-04-06 19:23:56 +0530

    new changes to trigger pipeline

--- DJB_EFModel/Program.cs
+++ DJB_EFModel/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddAppDI(builder.Configuration);
 
 var app = builder.Build();
 
-//Created Github actions try pushing
+//Created Github actions try pushing again
 app.UseSwagger();
 app.UseSwaggerUI();
 
