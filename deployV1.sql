1b0b97043a201ebb5f6bbf344b11814e288a4722
!@Murphy@!1 <tewari.nivedita@gmail.com>
2026-04-16 15:02:00 +0530

    update migrations design dll

---
+++ deployV1.sql
@@ -0,0 +1,69 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260318133520_dbinit'
+)
+BEGIN
+    CREATE TABLE [Customers] (
+        [Id] uniqueidentifier NOT NULL,
+        [Phone] nvarchar(max) NOT NULL,
+        [Email] nvarchar(max) NOT NULL,
+        [Address] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Customers] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260318133520_dbinit'
+)
+BEGIN
+    CREATE TABLE [Orders] (
+        [Id] uniqueidentifier NOT NULL,
+        [Date] nvarchar(max) NOT NULL,
+        [TotalPurchase] nvarchar(max) NOT NULL,
+        [productids] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Orders] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260318133520_dbinit'
+)
+BEGIN
+    CREATE TABLE [Products] (
+        [Id] uniqueidentifier NOT NULL,
+        [Name] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260318133520_dbinit'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260318133520_dbinit', N'8.0.0');
+END;
+GO
+
+COMMIT;
+GO
+
